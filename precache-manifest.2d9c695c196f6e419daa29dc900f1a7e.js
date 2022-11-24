ad7f2a2eb12bf39ac0aff47de478bf471847a768
Fan Guo <cfanguo@gmail.com>
2022-11-23 21:02:55 -0500

    Update 2022

---
+++ precache-manifest.2d9c695c196f6e419daa29dc900f1a7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a985826fc7fbfadd0904ee18feab376b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f41bbf200a6463f853e5",
+    "url": "/static/css/main.39b48d17.chunk.css"
+  },
+  {
+    "revision": "a989bf3e6b2234560d37",
+    "url": "/static/js/2.c69f14d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c69f14d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41bbf200a6463f853e5",
+    "url": "/static/js/main.6d208a7e.chunk.js"
+  },
+  {
+    "revision": "47251cb91e0357b8ff3b",
+    "url": "/static/js/runtime-main.a77159d8.js"
+  },
+  {
+    "revision": "19b764058238343551415926f6bf7d94",
+    "url": "/static/media/FanGuoResume.19b76405.pdf"
+  }
+]);
